a9340cef5b85a41f11118d9b6bcd016b415de498
Olakunle Abiola <ask4kunabi75@gmail.com>
2024-10-27 10:49:36 +0000

    chore: updates

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import time
 def print_current_time():
     current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     print(f"Current time: {current_time}")
+    time.sleep(5)
 
 if __name__ == "__main__":
     print_current_time()
