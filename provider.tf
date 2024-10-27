41bf530d7e4d09dbde1a2c6187ad77fc6f15d3ac
Olakunle Abiola <ask4kunabi75@gmail.com>
2024-10-27 01:31:19 +0100

    Set up CI pipeline for Docker build and push

--- provider.tf
+++
@@ -1,7 +0,0 @@
-provider "kubernetes" {
-  host = minikube_cluster.minikube_docker.host
-
-  client_certificate     = minikube_cluster.minikube_docker.client_certificate
-  client_key             = minikube_cluster.minikube_docker.client_key
-  cluster_ca_certificate = minikube_cluster.minikube_docker.cluster_ca_certificate
-}
